b32a38e6e5ed62ec5ae2aa4d67dc33bae388049c
Aleksayev1 <aleksayev@gmail.com>
2026-01-24 16:47:25 -0300

    fix: enable clickable links for English acupoints (LI, GB, etc) in AI Chat

--- src/components/AIRecommendationsPanel.tsx
+++ src/components/AIRecommendationsPanel.tsx
@@ -39,7 +39,7 @@ export const AIRecommendationsPanel: React.FC<AIRecommendationsPanelProps> = ({
   };
 
   const renderMessageContent = (content: string) => {
-    const pointRegex = /\b((?:IG|VB|VC|Ren|Du|P|C|TA|CS|F|R|BP|E|ID|B|YNSA)\s?-?\s?\d{1,2}[a-zA-Z]?|YNSA\s+[a-zA-Z]+)\b/gi;
+    const pointRegex = /\b((?:IG|LI|VB|GB|VC|Ren|CV|Du|GV|P|LU|C|HT|HE|TA|SJ|TB|CS|PC|F|LR|LV|R|KI|BP|SP|E|ST|ID|SI|B|BL|YNSA)\s?-?\s?\d{1,2}[a-zA-Z]?|YNSA\s+[a-zA-Z]+)\b/gi;
     const parts = content.split(pointRegex);
     return parts.map((part, i) => {
       if (part.match(pointRegex)) {
